87609e39e5ffa7ae5941b7814198f6e1d0d92d58
Shihabul Hasan <6shihab@gmail.com>
2022-08-17 03:17:11 +0600

    cpp codes

---
+++ cpp codes/Problem-9.1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+main()
+{
+    double mx_value = 8;
+    //double slot_bit = 156.25;
+    double Num_slot = 8;
+    cout<<"(a).";
+    double peak_deviation = 8*10;
+    cout<<"Peak frequency deviation = "<<peak_deviation<<endl;
+
+    cout<<"(b).";
+    double FM_index = (peak_deviation)/4;
+    cout<<"Frequency modulation index = "<<FM_index<<endl;
+
+    cout<<"(c).";
+    double PM_index = (10*8);
+    cout<<"Phase modulation index = "<<PM_index<<endl;
+
+}
